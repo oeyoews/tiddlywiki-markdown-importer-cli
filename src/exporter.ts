537f6355ff119da846f2b0dfc0d3a981439537d8
oeyoews <jyao4783@gmail.com>
2023-11-01 17:17:04 +0800

    update

--- src/exporter.ts
+++ src/exporter.ts
@@ -1,10 +1,8 @@
-import matter from 'gray-matter';
 import fs from 'fs';
-import path from 'path';
 import { log } from './lib/log';
-import slugify from 'slugify';
 import cliProgress from 'cli-progress';
 import chalk from 'chalk';
+import { getfile } from './exportfile';
 
 const baseurl = 'http://0.0.0.0:8000';
 const markdowntype = ['text/markdown', 'text/x-markdown'];
@@ -34,10 +32,6 @@ fs.mkdirSync(exportPath);
 const tiddlersjson = new URL(`/recipes/default/tiddlers.json`, baseurl);
 
 const markdownfiletitles: string[] = [];
-const user = 'oeyoews';
-const password = 'oeyoews';
-
-const headers = new Headers();
 
 // TODO: 验证是否需要密码
 // TODO: 是否验证成功
@@ -48,28 +42,7 @@ const headers = new Headers();
 // https://github.com/Jermolene/TiddlyWiki5/pull/7471
 // https://talk.tiddlywiki.org/t/question-how-to-render-json-instead-of-html-and-save-the-results-to-a-json-file/4910/15
 
-const loginurl = new URL(`status`, baseurl);
-
-fetch(loginurl, {}).then((res) => {
-  console.log(res);
-});
-
-// fetch(loginurl, {}).then((res) => {
-//   if (res.statusText === '  Authentication required') log('需要认证', 'red');
-// });
-
-fetch(tiddlersjson, {
-  // @ts-ignore
-  // data: {
-  //   user,
-  //   password,
-  //   tiddlyweb_redirect: '/status',
-  // },
-  // method: 'GET',
-  // headers: {
-  //   'X-Authenticated-User': 'oeyoews',
-  // },
-})
+fetch(tiddlersjson)
   .then((res) => {
     return res.json();
   })
@@ -85,7 +58,7 @@ fetch(tiddlersjson, {
   .then(() => {
     markdownfiletitles.forEach((title, index) => {
       progressBar.start(markdownfiletitles.length, index, { title });
-      getfile(title);
+      getfile(title, markdowntype, baseurl, fileExtension, exportPath);
       progressBar.update(index + 1, { title });
     });
   })
@@ -93,42 +66,3 @@ fetch(tiddlersjson, {
     progressBar.stop();
     log(`\n共有 ${markdownfiletitles.length} 个 tiddler`, 'cyan');
   });
-
-function getfile(title: string) {
-  const putTiddlerUrl = new URL(`/recipes/default/tiddlers/${title}`, baseurl);
-  fetch(putTiddlerUrl)
-    .then((res) => {
-      if (res.ok) return res.json();
-      return false;
-    })
-    .then((data) => {
-      if (!data) {
-        log(`获取 ${title} 失败`, 'red');
-        return;
-      }
-
-      const { text, fiedls, ...restfields } = data;
-
-      if (!markdowntype.includes(restfields?.type)) {
-        log(`${title} 不是 markdown 类型`, 'red');
-        return;
-      }
-
-      // 导出的字段使用原生格式, 不做任何修改, 尽量让第三方框架适配这个格式, 而不是在这里修改
-      const frontmatter = Object.assign({}, restfields, fiedls);
-
-      const content = matter.stringify({ content: `\n${text}` }, frontmatter);
-
-      const fileName = `${slugify(title, {
-        lower: true,
-        remove: /[*+~./()'"!:@]/g,
-      })}${fileExtension}`;
-      const targetfilename = path.join(exportPath, fileName);
-
-      if (!fs.existsSync(targetfilename)) {
-        fs.writeFileSync(targetfilename, content);
-      } else {
-        log(`${title} 已存在`, 'red');
-      }
-    });
-}
---
+++ src/exportfile.ts
@@ -0,0 +1,50 @@
+import { log } from './lib/log';
+import matter from 'gray-matter';
+import slugify from 'slugify';
+import path from 'path';
+import fs from 'fs';
+
+export function getfile(
+  title: string,
+  markdowntype: any[],
+  baseurl: string,
+  fileExtension: string,
+  exportPath: string,
+) {
+  const putTiddlerUrl = new URL(`/recipes/default/tiddlers/${title}`, baseurl);
+  fetch(putTiddlerUrl)
+    .then((res) => {
+      if (res.ok) return res.json();
+      return false;
+    })
+    .then((data) => {
+      if (!data) {
+        log(`获取 ${title} 失败`, 'red');
+        return;
+      }
+
+      const { text, fiedls, ...restfields } = data;
+
+      if (!markdowntype.includes(restfields?.type)) {
+        log(`${title} 不是 markdown 类型`, 'red');
+        return;
+      }
+
+      // 导出的字段使用原生格式, 不做任何修改, 尽量让第三方框架适配这个格式, 而不是在这里修改
+      const frontmatter = Object.assign({}, restfields, fiedls);
+
+      const content = matter.stringify({ content: `\n${text}` }, frontmatter);
+
+      const fileName = `${slugify(title, {
+        lower: true,
+        remove: /[*+~./()'"!:@]/g,
+      })}${fileExtension}`;
+      const targetfilename = path.join(exportPath, fileName);
+
+      if (!fs.existsSync(targetfilename)) {
+        fs.writeFileSync(targetfilename, content);
+      } else {
+        log(`${title} 已存在`, 'red');
+      }
+    });
+}
